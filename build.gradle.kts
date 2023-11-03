b62f52463e01d3a6bdcb71028322e61e2c255cff
Kevin.COJEAN <kevin.cojean@agysoft.fr>
2023-11-03 16:28:37 +0100

    enabling compilation for ultimate 2023.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.9.0"
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("org.jetbrains.changelog") version "1.3.1"
 }
 
@@ -55,44 +55,44 @@ tasks {
 //        targetCompatibility = "1.9"
 //    }
 
-    patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
-        pluginDescription.set(
-            File(projectDir, "README.md").readText().lines().run {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
+//    patchPluginXml {
+//        version.set(properties("pluginVersion"))
+//        sinceBuild.set(properties("pluginSinceBuild"))
+//        untilBuild.set(properties("pluginUntilBuild"))
+//        pluginDescription.set(
+//            File(projectDir, "README.md").readText().lines().run {
+//                val start = "<!-- Plugin description -->"
+//                val end = "<!-- Plugin description end -->"
+//
+//                if (!containsAll(listOf(start, end))) {
+//                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+//                }
+//                subList(indexOf(start) + 1, indexOf(end))
+//            }.joinToString("\n").run { markdownToHTML(this) }
+//        )
+//        changeNotes.set(provider { changelog.getLatest().toHTML() })
+//    }
+//
+//    runPluginVerifier {
+//        ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+//    }
+//
+//    publishPlugin {
+//        dependsOn("patchChangelog")
+//        token.set(System.getenv("PUGLISH_TOKEN"))
+//        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+//        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+//        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+//        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+//    }
 
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        )
-        changeNotes.set(provider { changelog.getLatest().toHTML() })
-    }
+//    compileKotlin {
+//        kotlinOptions.jvmTarget = "11"
+//    }
 
-    runPluginVerifier {
-        ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
-    }
-
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token.set(System.getenv("PUGLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
-    }
-
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
+//    compileTestKotlin {
+//        kotlinOptions.jvmTarget = "11"
+//    }
 }
 
 dependencies {
